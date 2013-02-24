5730e55daebc31c39de3527e3c324a9b6d28682c
GaussCheng <GaussCheng@gmail.com>
2013-02-24 17:22:27 +0800

    1.not use window.open to open a print note

--- customer_management/views.py
+++ customer_management/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 from customer_management.models import Customer
-
+from django.utils.translation import ugettext as _
 
 def print_customer_use_lht(request, customer_code):
     customer = Customer.objects.get(code=customer_code)
@@ -17,18 +17,19 @@ def print_customer_use_suer(request, customer_code):
     return render_to_response('customer_management/print_suer.html',
                               {'customer': customer})
     
-def print_customers(request, queryset, express_template_url):
+def print_customers(request, queryset, express_template_url, express_name):
     customers = queryset
     return render_to_response('customer_management/print_customers.html',
                               {'customers':customers,
-                               'template_url':express_template_url})
+                               'template_url':express_template_url,
+                               'express_name':express_name})
     
 def print_customers_use_lht(request, queryset):
-    return print_customers(request, queryset, 'print_using_lht')
+    return print_customers(request, queryset, 'print_using_lht', _("LHT"))
 
 def print_customers_use_sf(request, queryset):
-    return print_customers(request, queryset, 'print_using_sf')
+    return print_customers(request, queryset, 'print_using_sf', _("SF"))
 
 def print_customers_use_suer(request, queryset):
-    return print_customers(request, queryset, 'print_using_suer')
+    return print_customers(request, queryset, 'print_using_suer', _("SUER"))
 
